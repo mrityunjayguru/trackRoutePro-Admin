c9331cffb6bbf3c874737a4ec9acddef408d82e1
vikash-creative <code@creativedemonz.com>
2025-03-12 16:20:43 +0530

    cc

--- src/pages/Request/Component/HeaderCommon.tsx
+++ src/pages/Request/Component/HeaderCommon.tsx
@@ -3,36 +3,36 @@ import Request from "../Request";
 import ReviwDevices from "./ReviwDevices";
 
 function HeaderCommon() {
-  const [activeTab, setActiveTab] = useState("Add New"); // Default active tab
+  const [activeTab, setActiveTab] = useState("New Subscriber"); // Default active tab
   return (
     <>
       <div className="flex gap-5">
         <div
-          onClick={() => setActiveTab("Add New")}
+          onClick={() => setActiveTab("New Subscriber")}
           className={`font-medium text-xl cursor-pointer relative transition-colors duration-300 ease-in-out ${
-            activeTab === "Add New" ? "text-[#D9E821]" : "text-[#000000]"
+            activeTab === "New Subscriber" ? "text-[#D9E821]" : "text-[#000000]"
           }`}
         >
-          Add New
-          {activeTab === "Add New" && (
+          New Subscriber
+          {activeTab === "New Subscriber" && (
             <div className="absolute bottom-0 left-0 w-full h-[2px] bg-[#D9E821] transition-all duration-300 ease-in-out"></div>
           )}
         </div>
         <div
-          onClick={() => setActiveTab("Review")}
+          onClick={() => setActiveTab("New Device")}
           className={`font-medium text-xl cursor-pointer relative transition-colors duration-300 ease-in-out ${
-            activeTab === "Review" ? "text-[#D9E821]" : "text-[#000000]"
+            activeTab === "New Device" ? "text-[#D9E821]" : "text-[#000000]"
           }`}
         >
-          Review
-          {activeTab === "Review" && (
+          New Device
+          {activeTab === "New Device" && (
             <div className="absolute bottom-0 left-0 w-full h-[2px] bg-[#D9E821] transition-all duration-300 ease-in-out"></div>
           )}
         </div>
       </div>
       <div className="mt-3 border-b-2 border-[#D9E821]"></div>
 
-{activeTab=="Add New"?(<>
+{activeTab=="New Subscriber"?(<>
 <Request/>
 </>):(
   <ReviwDevices/>
