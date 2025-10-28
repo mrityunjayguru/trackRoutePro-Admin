dace7e3af29d473eb6a1ae5f884d9581b5b0c9f6
vikash-creative <code@creativedemonz.com>
2025-10-28 11:02:48 +0530

    commit

--- src/api/Repository.ts
+++ src/api/Repository.ts
@@ -1,16 +1,16 @@
 import { AxiosInstance } from "axios";
 import axios from "axios";
-  const jsessionId = localStorage.getItem("jsessionid");
 
 
 const Repository: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_URL,
 });
-  console.log(jsessionId,"jsessionIdjsessionId")  
 // Add a request interceptor to set the Authorization header
 Repository.interceptors.request.use( 
   (config) => {
     const token = localStorage.getItem("token");
+  const jsessionId = localStorage.getItem("jsessionid");
+
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
